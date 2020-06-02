1cbffec7a3ad5b7b0d1b58a720c3d8eddcde7c40
Sam Hao <s.hao@berkeley.edu>
2020-06-02 11:28:38 -0700

    use new clade definitions and modified nextstrain script

--- bin/alignment_assembly_stats.py
+++ bin/alignment_assembly_stats.py
@@ -126,9 +126,9 @@ if args.neighborvcf:
     stats = {**stats, **countVCF(args.neighborvcf, 'new_snps', 'new_mnps', 'new_indels', stats)}
 if args.clades:
     stats["clade"] = []
-    with open(args.clades) as f:
-        for line in f:
-            stats["clade"].append(line.strip())
+    clade = pd.read_csv(args.clades, sep='\t')
+    stats["clade"] = clade["clade"].values[0]
+    
 
 with open(args.out_prefix + ".stats.json", "w") as f:
     json.dump(stats, f, indent=2)
